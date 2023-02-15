59df49a3d3f2aa379e071cafd67e7cf025078e00
rhit-tianr <tianr@rose-hulman.edu>
2023-02-14 22:06:19 -0500

    Improvement & Refresh works

--- navigation/screens/BloodPressureScreen.tsx
+++ navigation/screens/BloodPressureScreen.tsx
@@ -128,7 +128,7 @@ export default function BloodPressureScreen({ route }: { route: any }) {
   }
 
   useEffect(() => {
-    fetchPatientData(id, password, BloodpressureData, setData, "blood pressure", ["systolic", "diastolic"]).then(() => init());
+    init().then(() => fetchPatientData(id, password, BloodpressureData, setData, "blood pressure", ["systolic", "diastolic"]));
   }, []);
 
   useEffect(() => {
